b0f3abd01eef79c31f768b655d5f7f191a169730
Edmarques Lima <edmarquessantosdelima@gmail.com>
2022-02-14 19:21:40 -0300

    feature: implements controllers pages

--- public/index.php
+++ public/index.php
@@ -1,13 +1,20 @@
 <?php
 
+require __DIR__ . '/../vendor/autoload.php';
+
+use Alura\Cursos\Controller\FormularioInsercao;
+use Alura\Cursos\Controller\ListarCursos;
+
 switch($_SERVER['PATH_INFO']) {
     case '/listar-cursos':
-        require 'listar-cursos.php';
+        $controlador = new ListarCursos;
+        $controlador->processaRequisicao();
         break;
     case '/novo-curso':
-        require 'formulario-novo-curso';
+        $controlador = new FormularioInsercao;
+        $controlador->processaRequisicao();
         break;
     default:
-        require 'error.php';
+        echo 'A página solicitada não existe!';
         break;
 }

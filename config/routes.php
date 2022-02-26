7e9a7001be188431863d57bb146f4fda59d4f9af
Edmarques Lima <edmarquessantosdelima@gmail.com>
2022-02-25 23:11:37 -0300

    feature: add login page

--- config/routes.php
+++ config/routes.php
@@ -5,7 +5,9 @@ use Alura\Cursos\Controller\{
     FormularioInsercao,
     ListarCursos,
     Persistencia,
-    FormularioEdicao
+    FormularioEdicao,
+    FormularioLogin,
+    RealizarLogin
 };
 
 return [
@@ -13,5 +15,7 @@ return [
     '/novo-curso' => FormularioInsercao::class,
     '/salvar-curso' => Persistencia::class,
     '/excluir-curso' => Exclusao::class,
-    '/alterar-curso' => FormularioEdicao::class
+    '/alterar-curso' => FormularioEdicao::class,
+    '/login' => FormularioLogin::class,
+    '/realiza-login' => RealizarLogin::class
 ];
---
+++ src/Controller/RealizarLogin.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Alura\Cursos\Controller;
+
+use Alura\Cursos\Entity\Usuario;
+use Alura\Cursos\Infra\EntityManagerCreator;
+
+class RealizarLogin implements InterfaceControladorRequisicao
+{
+    /**
+     * @var \Doctrine\Common\Persistence\ObjectRepository
+     */
+    private $repositorioDeUsuarios;
+
+    public function __construct()
+    {
+        $entityManager = (new EntityManagerCreator())->getEntityManager();
+        $this->repositorioDeUsuarios = $entityManager
+            ->getRepository(Usuario::class);
+    }
+
+    public function processaRequisicao(): void
+    {
+        $email = filter_input(
+            INPUT_POST,
+            'email',
+            FILTER_VALIDATE_EMAIL
+        );
+
+        if (!$email) {
+            echo 'Email inválido"';
+            return;
+        }
+
+        $senha = filter_input(
+            INPUT_POST,
+            'senha',
+            FILTER_SANITIZE_STRING
+        );
+
+        /** @var Usuario $usuario */
+        $usuario = $this->repositorioDeUsuarios
+            ->findOneBy(['email' => $email]);
+
+        if (!$usuario || !$usuario->senhaEstaCorreta($senha)) {
+            echo "Email ou senha inválidos!";
+            return;
+        }
+
+        header('Location: /listar-cursos');
+    }
+}
